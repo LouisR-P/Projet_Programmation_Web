4d0f9c19565113cc4b3bf5b82b8236131cd487e7
LouisR-P <louis.russopelosi@hotmail.fr>
2020-11-11 22:09:55 +0100

    Initialisation du projet de programmation web de 5A (Gestion centre équestre) avec spring initializr.

---
+++ src/main/java/com/projet5a/centre_equestre/CentreEquestreApplication.java
@@ -0,0 +1,13 @@
+package com.projet5a.centre_equestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentreEquestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentreEquestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet5a/centre_equestre/CentreEquestreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet5a.centre_equestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentreEquestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
